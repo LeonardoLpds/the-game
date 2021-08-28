a7202ce27c7b3c50f03337fc018f021eef20210a
Leonardo <leonardo.lpds@gmail.com>
2021-08-27 22:46:48 -0300

    Alterando sprites do heroi para um único ataque

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,6 +1,5 @@
 extends KinematicBody2D
 
-# Animations
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
@@ -13,13 +12,10 @@ enum {
 var state = MOVE
 
 # Move variables
-const WALK_SPEED = 130
-const ACCELERATION = 20
+const WALK_SPEED = 120
+const ACCELERATION = 15
 var velocity = Vector2.ZERO
 
-# Attack variables
-var AttackAnimation = 1
-var maxAttack = 2
 
 func _ready():
 	animationTree.active = true
@@ -35,12 +31,11 @@ func move_state():
 	var x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	var y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	var input = Vector2(x, y).normalized()
-	
+
 	if input != Vector2.ZERO:
 		animationTree.set("parameters/Idle/blend_position", input)
 		animationTree.set("parameters/Run/blend_position", input)
-		animationTree.set("parameters/Attack 1/blend_position", input)
-		animationTree.set("parameters/Attack 2/blend_position", input)
+		animationTree.set("parameters/Attack/blend_position", input)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input * WALK_SPEED, ACCELERATION)
 	else:
@@ -48,20 +43,15 @@ func move_state():
 		velocity = velocity.move_toward(Vector2.ZERO, ACCELERATION)
 
 	velocity = move_and_slide(velocity)
-	
+
 	if Input.is_action_just_pressed("attack"):
-		if $AttackTimer.time_left > 0 && AttackAnimation < maxAttack:
-			AttackAnimation = AttackAnimation + 1
-		else:
-			AttackAnimation = 1
-			
-		$AttackTimer.start()
 		state = ATTACK
 
 func attack_state():
 	velocity = Vector2.ZERO
-	animationState.travel("Attack " + str(AttackAnimation))
-		
-	
+	animationState.travel("Attack")
+	$SlashWeapon/Sword/AnimationPlayer.play("Attack Down")
+
+
 func reset_state():
 	state = MOVE
