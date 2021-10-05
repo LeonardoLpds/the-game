c34447cf4e6f24dfd2f85af261df2add86d63372
Leonardo Souza <leonardo.lpds@gmail.com>
2021-10-05 00:49:17 -0300

    Chamando skill tree na scene da GUI

--- src/ui/GUI.gd
+++ src/ui/GUI.gd
@@ -1,15 +1,17 @@
 extends CanvasLayer
 
 onready var inventory = preload("res://src/ui/iventory/scenes/Inventory.tscn").instance()
+onready var skill_tree = preload("res://src/ui/skills/skill_tree/SkillTree.tscn").instance()
 
-func _ready():
-	inventory.anchor_left = 0.25
-	inventory.anchor_top = 0.25
-	pass
-	
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("bag"):
 		if inventory.is_inside_tree():
 			remove_child(inventory)
 		else:
 			add_child(inventory)
+
+	if event.is_action_pressed("skills"):
+		if skill_tree.is_inside_tree():
+			remove_child(skill_tree)
+		else:
+			add_child(skill_tree)
